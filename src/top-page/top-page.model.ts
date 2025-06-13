a5334554da70dcfef38ea837482f8f0dc5736a92
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-06-13 23:12:14 +0300

    created top-page model schema

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +8,42 @@ export enum TopLevelCategory {
   Products,
 }
 
+@Schema({ id: true })
 export class TopPageModel {
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    default: new mongoose.Types.ObjectId(),
+  })
   _id: string;
+
+  @Prop()
   firstCategory: TopLevelCategory;
+  @Prop()
   secondCategory: string;
+  @Prop()
   title: string;
+  @Prop()
   category: string;
+  @Prop({
+    count: Number,
+    juniorSalary: Number,
+    middleSalary: Number,
+    seniorSalary: Number,
+  })
   hh?: {
     count: number;
     juniorSalary: number;
     middleSalary: number;
     seniorSalary: number;
   };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+  @Prop([{ title: String, description: String }])
+  advantages: { title: string; description: string }[];
+  @Prop()
   seoText: string;
+  @Prop()
   tagsTitle: string;
+  @Prop([String])
   tags: string[];
 }
+
+export const TopPageModelSchema = SchemaFactory.createForClass(TopPageModel);
