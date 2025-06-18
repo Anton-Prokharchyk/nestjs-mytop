d4295677770be091873cb45d03d4f27ddb5652a1
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-06-18 16:48:24 +0300

    added crud exceptions

---
+++ src/top-page/top-page.constants.ts
@@ -0,0 +1,4 @@
+export const TOP_PAGE_CANT_CREATE = 'TopPage cannot be created';
+export const TOP_PAGE_CANT_DELETE = 'TopPage cannot be deleted';
+export const TOP_PAGE_CANT_UPDATE = 'TopPage cannot be updated';
+export const TOP_PAGE_CANT_FOUND = 'TopPage not found';
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -3,7 +3,8 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -11,35 +12,49 @@ import {
 
 import { TopPage } from './top-page.model';
 import { TopPageService } from './top-page.service';
+import {
+  TOP_PAGE_CANT_CREATE,
+  TOP_PAGE_CANT_DELETE,
+  TOP_PAGE_CANT_FOUND,
+  TOP_PAGE_CANT_UPDATE,
+} from './top-page.constants';
 
 @Controller('top-page')
 export class TopPageController {
-  constructor(
-    private readonly topPageService: TopPageService, // Assuming TopPageService is defined and injected
-  ) {}
+  constructor(private readonly topPageService: TopPageService) {}
   @Post('create')
   async create(@Body() dto: Omit<TopPage, '_id'>): Promise<TopPage> {
     const created = await this.topPageService.create(dto);
+    if (!created) {
+      throw new HttpException(TOP_PAGE_CANT_CREATE, HttpStatus.BAD_REQUEST);
+    }
     return created;
   }
 
   @Get(':id')
   async findById(@Param('id') id: string) {
     const found = await this.topPageService.find(id);
+    if (!found) {
+      throw new HttpException(TOP_PAGE_CANT_FOUND, HttpStatus.NOT_FOUND);
+    }
     return found;
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
     const deleted = await this.topPageService.delete(id);
-    console.log('Deleted TopPage:', deleted);
+    if (!deleted) {
+      throw new HttpException(TOP_PAGE_CANT_DELETE, HttpStatus.BAD_REQUEST);
+    }
     return deleted;
   }
 
   @Patch(':id')
   async patch(@Param('id') id: string, @Body() dto: TopPage) {
     const updated = await this.topPageService.patch(id, dto);
-    console.log('Updated TopPage:', updated);
+    if (!updated) {
+      throw new HttpException(TOP_PAGE_CANT_UPDATE, HttpStatus.BAD_REQUEST);
+    }
     return updated;
   }
 }
