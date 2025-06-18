d158afb43e6d846643273e60593f2221109532f2
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-06-19 02:27:53 +0300

    added review crud

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -9,16 +9,34 @@ import {
   Post,
 } from '@nestjs/common';
 
-import { Review } from './review.model';
+import { Review, ReviewDocument } from './review.model';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
-  @Post('create')
-  async create(@Body() dto: Omit<Review, '_id'>) {}
+  constructor(private readonly reviewService: ReviewService) {}
 
-  @Get('byProduct/:productId')
-  async getByProduct(@Param('productId') productId: string) {}
+  @Post('create')
+  async create(@Body() dto: Review): Promise<ReviewDocument> {
+    return this.reviewService.create(dto);
+  }
+
+  @Get(':id')
+  async findByid(@Param('id') id: string) {
+    console.log(id);
+    return this.reviewService.find(id);
+  }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {}
+  async delete(@Param('id') id: string) {
+    return this.reviewService.delete(id);
+  }
+
+  @Patch(':id')
+  async patch(
+    @Param('id') id: string,
+    @Body() dto: Review,
+  ): Promise<ReviewDocument | null> {
+    return this.reviewService.patch(id, dto);
+  }
 }
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { ReviewController } from './review.controller';
 import { ReviewSchema, Review } from './review.model';
+import { ReviewService } from './review.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Review.name, schema: ReviewSchema }]),
   ],
   controllers: [ReviewController],
+  providers: [ReviewService],
 })
 export class ReviewModule {}
