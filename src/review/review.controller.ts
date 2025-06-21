249003ad54ac4640ece2db499c41880bd49364b4
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-06-21 19:57:07 +0300

    added error handling review module

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Delete,
   Get,
+  HttpException,
+  HttpStatus,
   Param,
   Patch,
   Post,
@@ -10,6 +12,11 @@ import {
 
 import { Review, ReviewDocument } from './review.model';
 import { ReviewService } from './review.service';
+import {
+  REVIEW_CANT_CREATE,
+  REVIEW_CANT_DELETE,
+  REVIEW_CANT_FOUND,
+} from './review.constants';
 
 @Controller('review')
 export class ReviewController {
@@ -17,32 +24,48 @@ export class ReviewController {
 
   @Post('create')
   async create(@Body() dto: Review): Promise<ReviewDocument> {
-    return this.reviewService.create(dto);
+    const createdReview: ReviewDocument = await this.reviewService.create(dto);
+    if (!createdReview)
+      throw new HttpException(REVIEW_CANT_CREATE, HttpStatus.BAD_REQUEST);
+    return createdReview;
   }
 
   @Get(':id')
   async findByid(@Param('id') id: string) {
-    console.log(id);
-    return this.reviewService.find(id);
+    const review = await this.reviewService.find(id);
+    if (!review)
+      throw new HttpException(REVIEW_CANT_FOUND, HttpStatus.NOT_FOUND);
+    return review;
   }
 
   @Get('product/:productId')
   async findByProductId(
     @Param('productId') productId: string,
   ): Promise<ReviewDocument[]> {
-    return this.reviewService.findByProductId(productId);
+    const reviews = await this.reviewService.findByProductId(productId);
+    if (!reviews)
+      throw new HttpException(REVIEW_CANT_FOUND, HttpStatus.BAD_REQUEST);
+
+    return reviews;
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-    return this.reviewService.delete(id);
+    const deletedReview = await this.reviewService.delete(id);
+    if (!deletedReview)
+      throw new HttpException(REVIEW_CANT_DELETE, HttpStatus.NOT_FOUND);
+
+    return deletedReview;
   }
 
   @Delete('product/:productId')
   async deleteByProductId(
     @Param('productId') productId: string,
   ): Promise<{ deletedCount: number }> {
-    return this.reviewService.deleteByProductId(productId);
+    const deletedCount = await this.reviewService.deleteByProductId(productId);
+    if (!deletedCount)
+      throw new HttpException(REVIEW_CANT_CREATE, HttpStatus.BAD_REQUEST);
+    return deletedCount;
   }
 
   @Patch(':id')
@@ -50,6 +73,9 @@ export class ReviewController {
     @Param('id') id: string,
     @Body() dto: Review,
   ): Promise<ReviewDocument | null> {
-    return this.reviewService.patch(id, dto);
+    const updatedReview = await this.reviewService.patch(id, dto);
+    if (!updatedReview)
+      throw new HttpException(REVIEW_CANT_CREATE, HttpStatus.BAD_REQUEST);
+    return updatedReview;
   }
 }
