da6392a73317180f9ea79bb82ef0dde360fc12c8
Anton.Prokharchyk <Anton.Prokharchyk@gmail.com>
2025-06-25 15:47:06 +0300

    added User model

---
+++ src/auth/user.model.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Date, HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ id: true, timestamps: true })
+export class User {
+  _id: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  createdAt: Date;
+
+  udpatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
